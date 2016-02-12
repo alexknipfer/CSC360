1c004bb34150c7125572cc4ad78cc9453e5c3fb2
alexknipfer <alexanderknipfer@gmail.com>
2016-02-12 23:00:11 +0000

    added project2, created initial node structure with linked list class

---
+++ Project2/project2.cpp
@@ -0,0 +1,102 @@
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <cstring>
+
+using namespace std;
+
+struct NodeType
+{
+  string firstName;
+  string lastName;
+  string address;
+  string city;
+  string state;
+  string zipCode;
+  
+  NodeType *NodePtr;
+};
+
+class LinkedListClass
+{
+  public:
+  LinkedListClass();
+    void InsertNode(NodeType);
+
+  private:
+    NodeType *StartPtr;
+};
+
+//******************************************************************************
+
+int main()
+{
+  NodeType Temp, Node;
+  
+    //instantiate object for customer
+  LinkedListClass LinkedList;
+  
+  ifstream inputFile("testdata.txt");
+  ofstream outputFile("output.txt");
+  
+  
+    //add, delete, change, or print records
+  char transactionType;
+  
+    //done reading data from file once Q is hit
+  char done = 'Q';
+  
+    //read in first character to figure out what to do with data
+  inputFile >> transactionType;
+  
+    //continue reading in data until a "Q" is read in
+  while(transactionType != done)
+  {
+      //check to see what to do with customer data
+    switch(transactionType)
+    {
+        //add customer if transaction type is A
+      case 'A':   inputFile >> ws;
+                  getline(inputFile, Node.firstName);
+                  inputFile >> ws;
+                  getline(inputFile, Node.lastName);
+                  inputFile >> ws;
+                  getline(inputFile, Node.address);
+                  inputFile >> ws;
+                  getline(inputFile, Node.city);
+                  inputFile >> ws;
+                  getline(inputFile, Node.state);
+                  inputFile >> ws;
+                  getline(inputFile, Node.zipCode);
+                  LinkedList.InsertNode(Node);
+      break;
+    }
+    
+      //read in next transaction type
+    inputFile >> transactionType;
+  }
+}
+
+//******************************************************************************
+
+LinkedListClass::LinkedListClass()
+{
+  StartPtr = NULL;
+}
+
+void LinkedListClass::InsertNode(NodeType Node)
+{
+  NodeType *newPtr, *PreviousPtr, *CurrentPtr;
+  
+  newPtr = new(NodeType);
+  
+  if(newPtr == NULL)
+  {
+    cout << "No memory available" << endl;
+  }
+  
+  else
+  {
+    
+  }
+}
