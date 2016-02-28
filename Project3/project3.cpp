9bb96428c34e00463f2eea1f8fe17131e6027d98
alexknipfer <alexanderknipfer@gmail.com>
2016-02-28 21:47:22 +0000

    added queue functions, adds final departures to vector

--- Project3/project3.cpp
+++ Project3/project3.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <iomanip>
 #include <cstring>
+#include <vector>
 
 using namespace std;
 
@@ -19,18 +20,58 @@ class LinkedQueueClass
     LinkedQueueClass();
     ~LinkedQueueClass();
     bool IsEmpty(){return ((front==NULL) ? true : false);}
-
+    bool IsFull();
+    CustomerData * First();
+    CustomerData * Last();
+    bool Add(CustomerData);
+    void PrintData(ofstream &);
+    void ProcessCustomer();
+    
   private:
     int count;
+    int totalTime;
+    vector<string> finalQueue;
     CustomerData *front;
     CustomerData *rear;
+    CustomerData *current;
 };
 
 //******************************************************************************
 
 int main()
 { 
-    return 0;
+  CustomerData customer;
+  LinkedQueueClass Queue;
+  
+  ifstream inputFile("data.txt");
+  ofstream outputFile("output.txt");
+  
+  inputFile >> customer.arrivalTime;
+  
+  while(customer.arrivalTime != -99)
+  {
+    inputFile >> ws;
+    getline(inputFile, customer.name);
+    inputFile >> ws;
+    inputFile >> customer.processingTime;
+    
+    Queue.Add(customer);
+    
+    inputFile >> customer.arrivalTime;
+  }
+  
+  Queue.ProcessCustomer();
+  
+  outputFile << "The order of customer arrival is:";
+  outputFile << "           ";
+  outputFile << "The order of customer departure is:" << endl;
+  outputFile << "---------------------------------";
+  outputFile << "          ";
+  outputFile << "---------------------------------" << endl;
+  
+  Queue.PrintData(outputFile);
+  
+  return 0;
 }
 
 //******************************************************************************
@@ -40,6 +81,7 @@ LinkedQueueClass::LinkedQueueClass()
   front = NULL;
   rear = NULL;
   count = 0;
+  totalTime = 0;
 }
 
 //******************************************************************************
@@ -57,7 +99,117 @@ LinkedQueueClass::~LinkedQueueClass()
 
 //******************************************************************************
 
-bool LinkedQueueClass()
+bool LinkedQueueClass::IsFull()
 {
-  
+  CustomerData *p;
+  p = new(CustomerData);
+  if(p == NULL)
+  {
+    delete p;
+    cout << "Out of memory. " << endl;
+    return true;
+  }
+  return false;
 }
+
+//******************************************************************************
+
+CustomerData * LinkedQueueClass::First()
+{
+  if(IsEmpty() )
+  {
+    cout << "Queue is Empty. " << endl;
+    return NULL;
+  }
+  return front;
+}
+
+//******************************************************************************
+
+CustomerData * LinkedQueueClass::Last()
+{
+  if(IsEmpty())
+  {
+    cout << "Queue is Empty. " << endl;
+    return NULL;
+  }
+  return rear;
+}
+
+//******************************************************************************
+
+bool LinkedQueueClass::Add(CustomerData NewNode)
+{
+  CustomerData *NewPtr;
+  NewPtr = new (CustomerData);
+  
+  if(IsFull())
+  {
+    return false;
+  }
+  
+  NewPtr -> arrivalTime = NewNode.arrivalTime;
+  NewPtr -> name = NewNode.name;
+  NewPtr -> processingTime = NewNode.processingTime;
+  totalTime += NewNode.processingTime;
+  NewPtr -> next = NULL;
+  
+  if(front == NULL)
+  {
+    front = NewPtr;
+  }
+  
+  else
+  {
+    rear -> next = NewPtr;
+  }
+  
+  rear = NewPtr;
+  count++;
+  return true;
+}
+
+//******************************************************************************
+
+
+void LinkedQueueClass::PrintData(ofstream &outputFile)
+{
+  cout << totalTime;
+  current = front;
+  int data = 0;
+    while(current != NULL)
+    {
+      outputFile << current -> name << "               |";
+      outputFile << setw(30) << finalQueue[data] << endl;
+      current = current -> next;
+      data++;
+    }
+}
+
+//******************************************************************************
+
+void LinkedQueueClass::ProcessCustomer()
+{
+  bool processed = false;
+  int processCount = count;
+  current = front;
+  
+  for(int x = 0; x < totalTime; x++)
+  {
+   while(current != NULL)
+    {
+      current -> processingTime -= 1;
+      if(current -> processingTime == 0)
+      {
+        finalQueue.push_back(current -> name);
+        processCount--;
+      }
+      
+      current = current -> next;
+      //cout << processCount << endl;
+    } 
+    current = front;
+  }
+    
+}
+
