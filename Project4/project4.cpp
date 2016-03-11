23d3fcdc136ac5d945e1ad10f05d3304865144cc
alexknipfer <alexanderknipfer@gmail.com>
2016-03-11 02:56:51 +0000

    print labels and adds to stack

--- Project4/project4.cpp
+++ Project4/project4.cpp
@@ -15,18 +15,20 @@ class StackClass
     StackClass();
     void Push(char);
     void Pop();
-    void Print();
+    void Print(string, ofstream &);
     bool RetrieveTop();
     bool IsEmpty(){return(TopPtr == NULL);}
     StackClass *GetTop(){return TopPtr;}
     bool IsFull();
-    void ProcessEquation(ifstream &);
+    void ProcessEquation(ifstream &, ofstream &);
     
   private:
     StackClass *TopPtr;
     StackClass *next;
     vector<char> postfixExpression;
+    vector <char> equationChar;
     char stackSymbol;
+    int infixCount;
 
 };
 
@@ -39,8 +41,14 @@ int main()
   ifstream inputFile("data.txt");
   ofstream outputFile("output.txt");
 
-  Stack.ProcessEquation(inputFile);
-  Stack.Print();
+  outputFile << "                         CONVERSION DISPLAY" << endl;
+  outputFile << "Infix Expression         POSTFIX Expression               Stack" << endl;
+  outputFile << "Contents                                               (Top to Bottom)" << endl;
+  outputFile << endl;
+
+  Stack.ProcessEquation(inputFile, outputFile);
+
+  //Stack.Print();
   return 0;
 }
 
@@ -50,15 +58,23 @@ StackClass::StackClass()
 {
   TopPtr = NULL;
   next = NULL;
+  infixCount = 0;
 }
 
 //******************************************************************************
 
-void StackClass::ProcessEquation(ifstream &inputFile)
+void StackClass::ProcessEquation(ifstream &inputFile, ofstream &outputFile)
 {
   string myEquation;
   inputFile >> myEquation;
   
+  for(int x = 0; x < myEquation.length(); x++)
+  {
+    equationChar.push_back(myEquation[x]);
+  }
+  
+  Print(myEquation, outputFile);
+
   while(myEquation != "X")
   {
     for(int x = 0; x < myEquation.length(); x++)
@@ -66,20 +82,18 @@ void StackClass::ProcessEquation(ifstream &inputFile)
       if(isdigit(myEquation[x]))
       {
         postfixExpression.push_back(myEquation[x]);
+        Print(myEquation, outputFile);
+        return;
       }
       
       else if(myEquation[x] == '(')
       {
         Push(myEquation[x]);
+        Print(myEquation, outputFile);
       }
     }
     inputFile >> myEquation;
   }
-  
-  /*for(int x = 0; x < postfixExpression.size(); x++)
-  {
-    cout << postfixExpression[x] << endl;
-  }*/
 }
 
 //******************************************************************************
@@ -96,16 +110,54 @@ void StackClass::Push(char symbol)
   return;
 }
 
-void StackClass::Print()
+//******************************************************************************
+
+void StackClass::Print(string equation, ofstream &outputFile)
 {
   StackClass *current;
   current = TopPtr;
-  
-  while(current != NULL)
+
+  for(int x = 0; x < infixCount; x++)
   {
-    cout << current -> stackSymbol << endl;
-    current = current -> next;
+    outputFile << " ";
   }
+  
+  for(int x = infixCount; x < equationChar.size(); x++)
+  {
+    outputFile << equationChar[x];
+  }
+  
+  if(postfixExpression.empty())
+  {
+    outputFile << "                       Empty                       ";
+  }
+  
+  else
+  {
+    outputFile << "                       ";
+    for(int x = 0; x < postfixExpression.size(); x++)
+    {
+      outputFile << postfixExpression[x];
+    }
+    outputFile << "                             ";
+  }
+  
+  if(IsEmpty())
+  {
+    outputFile << "Empty ";
+  }
+  
+  else
+  {
+    while(current != NULL)
+    {
+      outputFile << current -> stackSymbol;
+      current = current -> next;
+    }
+  }
+  
+  infixCount++;
+  outputFile << endl;
   return;
 }
 
