ef68df9d1aaf7fa9305707f701539906db049b50
alexknipfer <alexanderknipfer@gmail.com>
2016-03-11 15:02:56 +0000

    added pop function for stack

--- Project4/project4.cpp
+++ Project4/project4.cpp
@@ -68,15 +68,19 @@ void StackClass::ProcessEquation(ifstream &inputFile, ofstream &outputFile)
   string myEquation;
   inputFile >> myEquation;
   
+    //store string read in into character vector for printing
   for(int x = 0; x < myEquation.length(); x++)
   {
     equationChar.push_back(myEquation[x]);
   }
   
+    //print initial infix expression
   Print(myEquation, outputFile);
 
+    //loop through infix expressions until an X is read in then stop
   while(myEquation != "X")
   {
+      //go through infix expression operators and operands
     for(int x = 0; x < myEquation.length(); x++)
     {
       if(isdigit(myEquation[x]))
@@ -112,28 +116,55 @@ void StackClass::Push(char symbol)
 
 //******************************************************************************
 
+void StackClass::Pop()
+{
+  StackClass *p ;
+  if (IsEmpty() )
+	{
+    cout << " Stack is empty. " << endl;
+    cout << " Delete Operation Failed. " << endl;
+    delete p;
+    return;
+  }		
+              // Save element in the first node
+  //symbol = TopPtr -> stackSymbol; 
+              // Adjust Stack Top
+  p = TopPtr;	  
+  TopPtr = TopPtr -> next;
+
+  delete p;
+  return;
+}
+
+//******************************************************************************
+
 void StackClass::Print(string equation, ofstream &outputFile)
 {
   StackClass *current;
   current = TopPtr;
 
+    //print proper spacing to right align infix expression
   for(int x = 0; x < infixCount; x++)
   {
     outputFile << " ";
   }
   
+    //print infix expression
   for(int x = infixCount; x < equationChar.size(); x++)
   {
     outputFile << equationChar[x];
   }
   
+    //if the postfix expression consists of nothing print empty
   if(postfixExpression.empty())
   {
     outputFile << "                       Empty                       ";
   }
   
+    //print postfix expression vector
   else
   {
+      //create proper spacing before postfix expression
     outputFile << "                       ";
     for(int x = 0; x < postfixExpression.size(); x++)
     {
@@ -142,11 +173,13 @@ void StackClass::Print(string equation, ofstream &outputFile)
     outputFile << "                             ";
   }
   
+    //if stack is empty print empty
   if(IsEmpty())
   {
     outputFile << "Empty ";
   }
   
+    //print stack contents if it is not empty
   else
   {
     while(current != NULL)
@@ -156,6 +189,7 @@ void StackClass::Print(string equation, ofstream &outputFile)
     }
   }
   
+    //increment infix count for printing infix expression
   infixCount++;
   outputFile << endl;
   return;
