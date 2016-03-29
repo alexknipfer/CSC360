29cdea51b7aeab251cb64ea59ffa9897f4fb47c7
alexknipfer <alexanderknipfer@gmail.com>
2016-03-29 19:45:05 +0000

    now sorts by selling price in descending order using shell sort

--- Project5/project5.cpp
+++ Project5/project5.cpp
@@ -55,6 +55,7 @@ class InventoryClass
     void readData(ifstream &);
     void printData(ofstream &);
     void sortQuantityOnHand();
+    void sortSellingPrice();
   
   private:
     InventoryStruct inventoryData[50];
@@ -86,12 +87,26 @@ int main()
   
   outputFile << endl;
   
-    //sort inventory array in descending order by quantity on hand
+    //sort inventory array in descending order by quantity on hand using bubble sort
   myInventory.sortQuantityOnHand();
   
     //print labels for printing inventory
   outputFile << "The Inventory Array sorted in descending order according to the Quantity on Hand" << endl;
   outputFile << "using the Bubble Sort:" << endl;
+  outputFile << "Inventory  Item                    Quantity  Reorder  Cost of  Selling " << endl;
+  outputFile << " Number    Description             on hand   Number   Item     Price" << endl;
+  outputFile << "--------   -----------------       --------  -------  -------  -------" << endl;
+  
+    //print inventory array
+  myInventory.printData(outputFile);
+  
+  outputFile << endl;
+  
+    //sort inventory array in descending order by selling price using shell sort
+  myInventory.sortSellingPrice();
+  
+  outputFile << "The Inventory Array sorted in descending order according to the Selling Price" << endl;
+  outputFile << "using the Shell Sort:" << endl;
   outputFile << "Inventory  Item                    Quantity  Reorder  Cost of  Selling " << endl;
   outputFile << " Number    Description             on hand   Number   Item     Price" << endl;
   outputFile << "--------   -----------------       --------  -------  -------  -------" << endl;
@@ -201,6 +216,82 @@ void InventoryClass::sortQuantityOnHand()
       }
     }
   }
+}
+
+//******************************************************************************
+
+void InventoryClass::sortSellingPrice()
+{
+    //Receives - nothing
+    //Task - sort inventory array by selling price in descending order using shell sort
+    //Returns - nothing
+    
+    //create temp variables for all swaps
+  int i;
+  int tempInventoryNumber;
+  string tempItemDescription;
+  int tempQuantityOnHand;
+  int tempReorderNumber;
+  double tempCostOfItem;
+  double tempSellingPrice;
+  
+    //intialize flag variable
+  int flag = 1;
+  
+    //get distance of array
+  int d = inventoryCount;
+  
+    //keep sorting until complete (flag is true and d is always more than 1)
+  while(flag || (d > 1))
+  {
+    flag = 0;
+    
+      //set the stage value
+    d = (d + 1) / 2;
+    
+      //loop through inventory array
+    for(i = 0; i < (inventoryCount - d); i++)
+    {
+        //check of selling price is more than the one next to it
+        //sort into descending order
+      if(inventoryData[i + d].sellingPrice > inventoryData[i].sellingPrice)
+      {
+          //swap inventory number
+        tempInventoryNumber = inventoryData[i +d].inventoryNumber;
+        inventoryData[i + d].inventoryNumber = inventoryData[i].inventoryNumber;
+        inventoryData[i].inventoryNumber = tempInventoryNumber;
+        
+          //swap item description
+        tempItemDescription = inventoryData[i +d].itemDescription;
+        inventoryData[i + d].itemDescription = inventoryData[i].itemDescription;
+        inventoryData[i].itemDescription = tempItemDescription;
+        
+          //swap quantity on hand values
+        tempQuantityOnHand = inventoryData[i +d].quantityOnHand;
+        inventoryData[i + d].quantityOnHand = inventoryData[i].quantityOnHand;
+        inventoryData[i].quantityOnHand = tempQuantityOnHand;
+        
+          //swap reorder numbers
+        tempReorderNumber = inventoryData[i +d].reorderNumber;
+        inventoryData[i + d].reorderNumber = inventoryData[i].reorderNumber;
+        inventoryData[i].reorderNumber = tempReorderNumber;
+        
+          //swap cost of item
+        tempCostOfItem = inventoryData[i +d].costOfItem;
+        inventoryData[i + d].costOfItem = inventoryData[i].costOfItem;
+        inventoryData[i].costOfItem = tempCostOfItem;
+        
+          //swap selling price
+        tempSellingPrice = inventoryData[i +d].sellingPrice;
+        inventoryData[i + d].sellingPrice = inventoryData[i].sellingPrice;
+        inventoryData[i].sellingPrice = tempSellingPrice;
+        
+        //reset flag value
+        flag = 1;
+      }
+    }
+  }
+  
 }
 
 //******************************************************************************
